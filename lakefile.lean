f40f3ce03311d490478f092fa31a2b126f6c2213
Paul Mure <paulmure2000@gmail.com>
2024-09-11 15:21:18 -0400

    refactor

--- lakefile.lean
+++ lakefile.lean
@@ -4,14 +4,11 @@ open Lake DSL
 package «spacetalk» where
   -- add any package configuration options here
 
--- require batteries from
---   git "https://github.com/leanprover-community/batteries" @ "main"
+require batteries from
+  git "https://github.com/leanprover-community/batteries" @ "main"
 
--- require mathlib from
---   git "https://github.com/leanprover-community/mathlib4.git"
-
-require smt from
-  git "https://github.com/ufmg-smite/lean-smt.git" @ "main"
+require mathlib from
+  git "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
 lean_lib «Spacetalk» where
