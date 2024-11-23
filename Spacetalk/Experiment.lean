31f9a329f71a39e96e6eaa25a39bb683688a72c0
Paul Mure <paulmure2000@gmail.com>
2024-11-23 11:54:28 -0500

    finish compiler

--- Spacetalk/Experiment.lean
+++ Spacetalk/Experiment.lean
@@ -18,10 +18,6 @@ namespace Arith
     | plus : Eval env e1 x
       → Eval env e2 y
       → Eval env (.plus e1 e2) (x + y)
-
-  def Exp.notConst : Exp → Bool
-    | .const _ => false
-    | _ => true
 end Arith
 
 namespace Df
@@ -50,7 +46,7 @@ namespace Df
 
   inductive NodeOp
     | input : List Tag → NodeOp
-    | output
+    | output : Inp → NodeOp
     | plus : Inp → Inp → List Tag → NodeOp
 
   structure Node where
@@ -80,6 +76,9 @@ namespace Df
     | input : {nid : Nid} → {ts : List Tag} → {s : State}
       → (h : (s nid.fst) ≠ [])
       → Node.Step ⟨nid, .input ts⟩ s (s ↤ nid.fst ↦↦ ⟨(s nid.fst).head h, ts⟩)
+    | outputConst : {nid : Nid} → {s : State} → {c : Ty}
+      → s nid.fst = []
+      → Node.Step ⟨nid, .output (.const c)⟩ s (s ↦ ⟨c, nid.fst⟩)
     | plusConstConst : {nid : Nid} → {ts : List Tag} → {s : State} → {e1 e2 : Inp}
       → (h1 : e1 = .const x) → (h2 : e2 = .const y)
       → Node.Step ⟨nid, .plus e1 e2 ts⟩ s (s ↦↦ ⟨x + y, ts⟩)
@@ -102,16 +101,23 @@ namespace Df
       → DFG.Step dfg s1 s3
 
   theorem DFG.confluence {a b c : State} (dfg : DFG)
-    : dfg.Step a b → dfg.Step a c → ∃ d, dfg.Step b d ∧ dfg.Step c d := by
-    sorry
+    : dfg.Step a b → dfg.Step a c → ∃ d, dfg.Step b d ∧ dfg.Step c d
+    | .refl, .refl => ⟨a, .intro .refl .refl⟩
+    | .refl, s => ⟨c, .intro s .refl⟩
+    | s, .refl => ⟨b, .intro .refl s⟩
+    | .step n1 n1s gs1, .step n2 n2s gs2 => by
+      cases n1s <;> cases n2s
+      all_goals sorry
 
   inductive Node.final : Node → State → Prop
     | input : {nid : Nid} → {ts : List Tag} → {s : State}
       → s nid.fst = []
       → Node.final ⟨nid, .input ts⟩ s
-    | output : {nid : Nid} → {s : State} → {ret : Ty}
+    | outputConst : {nid : Nid} → {s : State} → {c : Ty}
+      → Node.final ⟨nid, .output (.const c)⟩ s
+    | outputPort : {nid : Nid} → {s : State} → {ret : Ty}
       → s nid.fst = [ret]
-      → Node.final ⟨nid, .output⟩ s
+      → Node.final ⟨nid, .output .port⟩ s
     | plusConstConst : {nid : Nid} → {ts : List Tag} → {s : State} → {x y : Ty}
       → Node.final ⟨nid, .plus (.const x) (.const y) ts⟩ s
     | plusPortConst : {nid : Nid} → {ts : List Tag} → {s : State} → {y : Ty}
@@ -134,24 +140,89 @@ namespace Compiler
   structure MarkedDFG where
     dfg : Df.DFG
     vars : VarMap
-    ret : Tag
+    ret : Df.Tag
 
-  def compileAux (maxNid : Df.Nid) (e : Arith.Exp) (h : e.notConst = true) : MarkedDFG :=
-    match e, h with
-    | .var s, _ =>
-      sorry
-    | .plus e1 e2, _ => sorry
+  def getRetInp (dfg : Df.DFG) (nid : Df.Nid) : Df.Inp :=
+    match dfg.find? (·.id = nid) with
+    | some ⟨_, .output i⟩ => i
+    | _ => .port
 
-  def compile (e : Arith.Exp) (h : e.notConst = true) : MarkedDFG :=
-    compileAux 0 e h
+  def Df.Node.updateReturn (ret : Df.Tag) (newRet : Df.Tag) (node : Df.Node) : Df.Node :=
+    let replace t := if t = ret then newRet else t
+    {node with op :=
+      match node.op with
+      | .input ts => .input (ts.map replace)
+      | .output i => .output i
+      | .plus e1 e2 ts => .plus e1 e2 (ts.map replace)
+    }
+
+  def merge (g1 : MarkedDFG) (g2 : MarkedDFG) (nid : Df.Nid)
+    : Df.DFG × VarMap × Df.Inp × Df.Inp :=
+    let (dfg, vars) := g2.vars.fold (
+      λ (dfg, vars) s t2 =>
+        match dfg.find? (·.id = t2.node) with
+        | some ⟨_, .input ts2⟩ =>
+          match g1.vars.get? s with
+          | some t1 =>
+            let dfg := (dfg.filter (·.id ≠ t2.node)).map (
+              λ node =>
+                if node.id = t1.node then
+                  match node.op with
+                  | .input ts1 => {node with op := .input (ts1 ++ ts2)}
+                  | _ => node
+                else
+                  node
+            )
+            (dfg, vars)
+          | none => (dfg, vars.insert s t2)
+        | _ => (dfg, vars)
+    ) (g1.dfg ++ g2.dfg, g1.vars)
+
+    -- Find the input edge of the orignal output nodes
+    let ret1 := getRetInp dfg g1.ret.node
+    let ret2 := getRetInp dfg g2.ret.node
+
+    -- Update links to the original output nodes
+    let updateReturn dfg ret := match ret with
+      | .port => dfg.map (Df.Node.updateReturn g1.ret nid.fst)
+      | .const _ => dfg
+    let dfg := updateReturn dfg ret1
+    let dfg := updateReturn dfg ret2
+
+    -- Remove all output nodes
+    let dfg := dfg.filter (λ node => match node.op with | .output _ => false | _ => true)
+
+    (dfg, vars, ret1, ret2)
+
+  def compileAux (maxNid : Df.Nid) : Arith.Exp → MarkedDFG × Df.Nid
+    | .const c =>
+      let dfg := [⟨maxNid, .output (.const c)⟩]
+      (⟨dfg, .empty, maxNid.fst⟩, maxNid + 1)
+    | .var s =>
+      let inpId := maxNid
+      let outId := maxNid + 1
+      let dfg := [⟨inpId, .input []⟩, ⟨outId, .output .port⟩]
+      let vars := Std.HashMap.empty.insert s maxNid.fst
+      (⟨dfg, vars, outId.fst⟩, maxNid + 2)
+    | .plus e1 e2 =>
+      let (e1, maxNid) := compileAux maxNid e1
+      let (e2, maxNid) := compileAux maxNid e2
+      let plusId := maxNid
+      let outId := maxNid + 1
+      let (dfg, vars, i1, i2) := merge e1 e2 plusId
+      let dfg := ⟨plusId, .plus i1 i2 [outId.fst]⟩ :: ⟨outId, .output .port⟩ :: dfg
+      (⟨dfg, vars, outId.fst⟩, maxNid + 2)
+
+  def compile (e : Arith.Exp) : MarkedDFG :=
+    (compileAux 0 e).fst
 
   def initialState (env : Arith.Env) (vars : VarMap) : Df.State :=
     vars.fold (λ s name tag => s ↦ ⟨env name, tag⟩) Df.State.empty
 
   theorem compile_correct {e : Arith.Exp} {env : Arith.Env} {v : Ty}
-    (h : e.notConst = true) (eval : Arith.Eval env e v) (finalState : Df.State) :
-    Df.DFG.Step (compile e h).dfg (initialState env (compile e h).vars) finalState
-      ∧ finalState (compile e h).ret = [v] :=
+    (eval : Arith.Eval env e v) (finalState : Df.State) :
+    Df.DFG.Step (compile e).dfg (initialState env (compile e).vars) finalState
+      ∧ finalState (compile e).ret = [v] :=
     sorry
 
 end Compiler
