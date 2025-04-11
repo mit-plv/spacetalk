9fe401500509fe0279501490d441aa2667e1e811
Paul Mure <paulmure2000@gmail.com>
2025-04-11 16:38:25 -0400

    prove compiler

--- Spacetalk/SimpleArith.lean
+++ Spacetalk/SimpleArith.lean
@@ -7,12 +7,14 @@ theorem Vector.cast_toArray {h : arr.size = n} {heq : n = m} : (heq ▸ ⟨arr, 
   subst h heq
   simp_all only
 
-def Vector.nil_append : ∀ vNil : Vector α 0, ∀ v : Vector α n, vNil.append v = (Nat.zero_add n).symm ▸ v := by
+def Vector.nil_append : ∀ vNil : Vector α 0, ∀ v : Vector α n, vNil ++ v = (Nat.zero_add n).symm ▸ v := by
   intro vNil v
   obtain ⟨_, h_size⟩ := vNil
   simp_rw [Array.size_eq_zero_iff.mp h_size]
-  simp only [append, Array.empty_append, mk_eq]
+  simp only [HAppend.hAppend, append, mk_eq]
   rw [Vector.cast_toArray]
+  rw [Append.append, Array.instAppend]
+  simp only [Array.append_eq_append, Array.empty_append]
 
 def Vector.split (v : Vector α (n + m)) : Vector α n × Vector α m :=
   have h_min_eq : min n (n + m) = n := Nat.min_add_right
@@ -20,63 +22,72 @@ def Vector.split (v : Vector α (n + m)) : Vector α n × Vector α m :=
   (h_min_eq ▸ v.take n, h_minus_eq ▸ v.drop n)
 
 theorem Vector.plus_eq_append
-  : ∀ v : Vector α (n + m), ∃ (v1 : Vector α n) (v2 : Vector α m), v = v1.append v2 := by
+  : ∀ v : Vector α (n + m), ∃ (v1 : Vector α n) (v2 : Vector α m), v = v1 ++ v2 := by
   intro v
   have h_min_eq : min n (n + m) = n := Nat.min_add_right
   have h_minus_eq : n + m - n = m := by omega
   exists (h_min_eq ▸ v.take n)
   exists (h_minus_eq ▸ v.drop n)
   obtain ⟨arr, h⟩ := v
-  simp only [append, take_eq_extract, extract_mk, Nat.sub_zero, cast_toArray, drop_eq_cast_extract,
-    cast_mk, Array.extract_append_extract, Nat.zero_le, inf_of_le_left, Nat.le_add_right,
-    sup_of_le_right, eq_mk]
-  simp_all only [Nat.le_add_right, inf_of_le_left, Nat.add_sub_cancel_left]
-  ext i hi₁ hi₂ : 1
-  · simp_all only [Array.size_extract, min_self, Nat.sub_zero]
-  · simp_all only [Array.getElem_extract, Nat.zero_add]
+  simp only [take_eq_extract, extract_mk, Nat.sub_zero, drop_eq_cast_extract, cast_mk, mk_eq,
+    toArray_append, cast_toArray, Array.extract_append_extract, Nat.zero_le, inf_of_le_left,
+    Nat.le_add_right, sup_of_le_right]
+  rw [←h]
+  simp only [Array.extract_size]
 
-theorem Vector.append_split_left_eq {v1 : Vector α n} {v2 : Vector α m} : (v1.append v2).split.1 = v1 := by
+theorem Vector.append_split_left_eq {v1 : Vector α n} {v2 : Vector α m} : (v1 ++ v2).split.1 = v1 := by
   obtain ⟨arr1, h1⟩ := v1
-  simp only [split, append, take_eq_extract, extract_mk, Nat.sub_zero, Array.extract_append,
-    Nat.zero_le, Nat.sub_eq_zero_of_le, drop_eq_cast_extract, cast_mk, eq_mk]
-  rw [Vector.cast_toArray]
-  subst h1
-  simp_all only [Array.extract_size, Nat.sub_self, Array.append_right_eq_self, Array.extract_eq_empty_iff,
-    size_toArray, Nat.zero_le, inf_of_le_left, le_refl]
-
-theorem Vector.append_split_right_eq {v1 : Vector α n} {v2 : Vector α m} : (v1.append v2).split.2 = v2 := by
   obtain ⟨arr2, h2⟩ := v2
-  simp only [split, append, take_eq_extract, extract_mk, Nat.sub_zero, Array.extract_append,
-    Nat.zero_le, Nat.sub_eq_zero_of_le, drop_eq_cast_extract, cast_mk, eq_mk]
-  rw [Vector.cast_toArray]
+  subst h1
   subst h2
-  simp_all only [size_toArray, Nat.sub_self, Nat.add_sub_cancel_left, Array.extract_size, Array.append_left_eq_self,
-    Array.extract_eq_empty_iff, Nat.le_add_right, inf_of_le_right, le_refl]
+  simp only [split, mk_append_mk, take_eq_extract, extract_mk, Nat.sub_zero, Array.extract_append,
+    Array.extract_size, Nat.zero_le, Nat.sub_eq_zero_of_le, Nat.sub_self, drop_eq_cast_extract,
+    Array.extract_size_left, Nat.add_sub_cancel_left, Array.empty_append, cast_mk, eq_mk,
+    cast_toArray, Array.append_right_eq_self, Array.extract_eq_empty_iff, inf_of_le_left, le_refl]
+
+theorem Vector.append_split_right_eq {v1 : Vector α n} {v2 : Vector α m} : (v1 ++ v2).split.2 = v2 := by
+  obtain ⟨arr1, h1⟩ := v1
+  obtain ⟨arr2, h2⟩ := v2
+  subst h1
+  subst h2
+  simp only [split, mk_append_mk, take_eq_extract, extract_mk, Nat.sub_zero, Array.extract_append,
+    Array.extract_size, Nat.zero_le, Nat.sub_eq_zero_of_le, Nat.sub_self, drop_eq_cast_extract,
+    Array.extract_size_left, Nat.add_sub_cancel_left, Array.empty_append, cast_mk, eq_mk,
+    cast_toArray]
 
 theorem Vector.append_split_map_left_eq {v1 : Vector α n} {v2 : Vector α m} {f : α → β}
-  : ((v1.append v2).map f).split.1 = v1.map f := by
+  : ((v1 ++ v2).map f).split.1 = v1.map f := by
   obtain ⟨arr1, h1⟩ := v1
+  obtain ⟨arr2, h2⟩ := v2
   subst h1
-  simp only [split, append, map_mk, Array.map_append, take_eq_extract, extract_mk, Nat.sub_zero,
-    Array.extract_append, Array.size_map, Nat.zero_le, Nat.sub_eq_zero_of_le, Nat.sub_self,
-    drop_eq_cast_extract, Nat.add_sub_cancel_left, cast_mk, eq_mk, cast_toArray]
+  subst h2
+  simp only [split, mk_append_mk, map_mk, Array.map_append, take_eq_extract, extract_mk,
+    Nat.sub_zero, Array.extract_append, Array.size_map, Nat.zero_le, Nat.sub_eq_zero_of_le,
+    Nat.sub_self, drop_eq_cast_extract, Nat.add_sub_cancel_left, cast_mk, eq_mk, cast_toArray]
   rw [Array.map_extract.symm]
   simp only [Array.extract_size, Array.append_right_eq_self, Array.extract_eq_empty_iff,
-    Array.size_map, size_toArray, Nat.zero_le, inf_of_le_left, le_refl]
+    Array.size_map, Nat.zero_le, inf_of_le_left, le_refl]
 
 theorem Vector.append_split_map_right_eq {v1 : Vector α n} {v2 : Vector α m} {f : α → β}
-  : ((v1.append v2).map f).split.2 = v2.map f := by
+  : ((v1 ++ v2).map f).split.2 = v2.map f := by
   obtain ⟨arr1, h1⟩ := v1
-  subst h1
   obtain ⟨arr2, h2⟩ := v2
+  subst h1
   subst h2
-  simp only [split, append, map_mk, Array.map_append, take_eq_extract, extract_mk, Nat.sub_zero,
-    Array.extract_append, Array.size_map, Nat.zero_le, Nat.sub_eq_zero_of_le, Nat.sub_self,
-    drop_eq_cast_extract, Nat.add_sub_cancel_left, cast_mk, eq_mk, cast_toArray]
+  simp only [split, mk_append_mk, map_mk, Array.map_append, take_eq_extract, extract_mk,
+    Nat.sub_zero, Array.extract_append, Array.size_map, Nat.zero_le, Nat.sub_eq_zero_of_le,
+    Nat.sub_self, drop_eq_cast_extract, Nat.add_sub_cancel_left, cast_mk, eq_mk, cast_toArray]
   rw [Array.map_extract.symm]
   simp only [Array.extract_size_left, List.map_toArray, List.map_nil, Array.empty_append,
     Array.extract_eq_self_iff, Array.size_map, List.length_eq_zero_iff, Array.toList_eq_nil_iff,
     le_refl, and_self, or_true]
+
+theorem Vector.append_assoc {v1 : Vector α n} {v2 : Vector α m} {v3 : Vector α k} : v1 ++ v2 ++ v3 = (Nat.add_assoc n m k) ▸ (v1 ++ (v2 ++ v3)) := by
+  obtain ⟨arr1, h1⟩ := v1
+  obtain ⟨arr2, h2⟩ := v2
+  obtain ⟨arr3, h3⟩ := v3
+  simp only [mk_append_mk, Array.append_assoc, mk_eq]
+  rw [Vector.cast_toArray]
 
 inductive Exp : Nat → Type
   | val : Nat → Exp 0
@@ -138,11 +149,11 @@ def Exp.compile (e : Exp n) : Graph α n :=
   e.compileC λ out => .nodeClosed .id #v[out]
 
 theorem compileC_correct
-  : ∀ e : Exp nExp, ∀ f : Stream' Nat → Graph _ nCont,
+  : ∀ e : Exp nExp, ∀ c : Stream' Nat → Graph _ nCont,
     ∀ vInpExp : Vector (Stream' Nat) nExp, ∀ vInpCont : Vector (Stream' Nat) nCont,
-    (f (e.denote vInpExp)).denote vInpCont = (e.compileC f).denote (vInpExp.append vInpCont) := by
-  intro e f vInpExp vInpCont
-  induction e generalizing f with
+    (c (e.denote vInpExp)).denote vInpCont = (e.compileC c).denote (vInpExp ++ vInpCont) := by
+  intro e c vInpExp vInpCont
+  induction e generalizing c nCont with
   | val v =>
     -- Get rid of all castings first
     simp only [Exp.compileC, graph_cast_denote]
@@ -158,11 +169,22 @@ theorem compileC_correct
       Node.denote, Vector.append_split_left_eq, Vector.append_split_right_eq]
     rfl
   | plus e1 e2 ih1 ih2 =>
+    -- Get rid of all casts
     simp only [Exp.compileC, graph_cast_denote]
     obtain ⟨vInp1, vInp2, h_inp⟩ := Vector.plus_eq_append vInpExp
     rw [h_inp]
+    conv =>
+      rhs
+      arg 2
+      simp only [Vector.append_assoc, Eq.rec_eq_cast, cast_cast, cast_eq]
+    -- No more casts!
+    rw [←(ih1 _ vInp1)]
+    rw [←(ih2 _ vInp2)]
+    simp only [Graph.denote, Node.denote, Vector.getElem_mk, List.getElem_toArray,
+      List.getElem_cons_zero, List.getElem_cons_succ]
+    unfold Stream'.zip
     unfold Exp.denote
     simp only [Exp.eval, Vector.packStreams, Vector.append_split_map_left_eq,
       Vector.append_split_map_right_eq]
-
-    sorry
+    subst h_inp
+    rfl
