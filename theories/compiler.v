2c0bf1cca5746f40c91dfdfdb65d1a499eee5664
Paul Mure <paulmure@30-9-114.wireless.csail.mit.edu>
2025-05-09 16:29:38 -0400

    ctree

--- theories/compiler.v
+++ theories/compiler.v
@@ -1,7 +1,29 @@
-
-Inductive Ty : Type :=
-    | unit
-    | bool
-    | nat.
+From Coq Require Import Unicode.Utf8.
+Require Import ExtLib.Structures.Monad.
 
 
+CoInductive ctree {E : Type → Type} {R : Type} : Type :=
+| ret : R → ctree
+| tau : ctree → ctree
+| vis {X} : E X → (X → ctree) → ctree
+| zero : ctree
+| choice : ctree → ctree → ctree.
+
+Infix "⊕" := choice (at level 80, right associativity).
+
+Arguments ctree : clear implicits.
+
+CoFixpoint ctree_bind {E} {R1 R2} (ct : ctree E R1) (f : R1 → ctree E R2) : ctree E R2 :=
+match ct with
+| ret v => f v
+| tau ct => tau (ctree_bind ct f)
+| vis e k => vis e (λ x, ctree_bind (k x) f)
+| zero => zero
+| p ⊕ q => (ctree_bind p f) ⊕ (ctree_bind q f)
+end.
+
+Instance ctree_Monad {E} : Monad (ctree E) :=
+{
+    ret _ v := ret v;
+    bind _ _ ct f := ctree_bind ct f
+}.
